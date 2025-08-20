2053b61076fc499738a55f85a78b8708a4ed2b29
priyanka priyadarshini rout <pprout143@gmail.com>
2025-08-20 10:30:23 +0530

    add docker

--- JyotiSoftware_pvt_ltd2/src/main/java/com/jyoti/JyotiSoftwarePvtLtdApplication.java
+++ JyotiSoftware_pvt_ltd2/src/main/java/com/jyoti/JyotiSoftwarePvtLtdApplication.java
@@ -3,7 +3,7 @@ package com.jyoti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.jyoti")
+@SpringBootApplication
 public class JyotiSoftwarePvtLtdApplication {
 
 	public static void main(String[] args) {
